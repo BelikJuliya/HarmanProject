7ed84d5792735222c0c776333c62cf2c68d8da5c
Yuliya <ulya962009@gmail.com>
2020-03-27 12:13:00 +0300

    GridFragment added on to Activity 2, but smth goes wrong in GridFragment.java onCreateView when I click on button which supposed to show Activity2

--- app/src/main/java/android/example/harmanproject/Activity2.java
+++ app/src/main/java/android/example/harmanproject/Activity2.java
@@ -3,14 +3,24 @@ package android.example.harmanproject;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import java.util.ArrayList;
 
 public class Activity2 extends AppCompatActivity {
 
+    GridFragment gridFragment;
+    FragmentTransaction gridTrans1;
+    //FragmentTransaction
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);
+
+        gridFragment = new GridFragment();
+        gridTrans1 = getSupportFragmentManager().beginTransaction();
+        gridTrans1.add(R.id.container, gridFragment);
+        gridTrans1.commit();
     }
 
     public static ArrayList<String> generateValues() {
--- app/src/main/java/android/example/harmanproject/GridFragment.java
+++ app/src/main/java/android/example/harmanproject/GridFragment.java
@@ -1,15 +1,56 @@
-//package android.example.harmanproject;
-//
-//import android.os.Bundle;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.GridView;
-//import android.widget.TextView;
-//
-//import androidx.fragment.app.Fragment;
-//
-//public class GridFragment extends Fragment {
+package android.example.harmanproject;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+//    private TextView textView;
+//    private GridAdapter gridAdapter;
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        textView = findViewById(R.id.list_item_text1);
+//        final GridView gridView = (GridView) findViewById(R.id.grid_view);
+//        gridAdapter = new GridAdapter(getApplicationContext(), generateValues());
+//        gridView.setAdapter(gridAdapter);
+
+
+public class GridFragment extends Fragment {
+    Context context;
+    GridView gridView;
+    GridAdapter gridAdapter;
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savesInstanceState) {
+        context = inflater.getContext(); //what does it do?
+        view =  inflater.inflate(R.layout.grid_fragment, container, false);
+        gridView = getView().findViewById(R.id.grid_view);
+        gridAdapter = new GridAdapter(context, Activity2.generateValues());
+        gridView.setAdapter(gridAdapter);
+        return view;
+    }
+}
+
+
+
+//        View view = inflater.inflate(R.layout.grid_view_element, container, false);
+//        gridAdapter = new GridAdapter(context, Activity2.generateValues());
+//        gridView = getView().findViewById(R.id.grid_item);
+//        gridView.setAdapter(gridAdapter);
+//        return view;
+//    }
+//}
+
+
+
 //
 //    // consolidated project
 //
