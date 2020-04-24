7a87e1afeb3104e4620225baaa851e6b3bf2229b
Yuliya <ulya962009@gmail.com>
2020-04-24 12:09:13 +0300

    It builds a route from phone location to picture from gallery, new metadata added to the textView

--- app/src/main/java/android/example/harmanproject/Activity3.java
+++ app/src/main/java/android/example/harmanproject/Activity3.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,7 +16,9 @@ import androidx.exifinterface.media.ExifInterface;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
 
 import java.io.File;
 import java.io.IOException;
@@ -47,15 +50,34 @@ public class Activity3 extends AppCompatActivity {
         try {
             File currentImage = new File(exampleElement.getPath());
             Metadata imageMetadata = ImageMetadataReader.readMetadata(currentImage);
+            for (Directory obj : imageMetadata.getDirectories()) {
+                Log.e("metadata", obj.getName());
+                for (Tag tag : obj.getTags()) {
+                    Log.e("metadata", tag.getDescription());
+                    Log.e("metadata", tag.getTagName());
+                    if (tag.getTagName().equals("GPS Latitude")) {
+                        System.out.println("here " + tag.getDescription());
+                    }
+                }
+            }
 
             ExifInterface exif = new ExifInterface(currentImage.getAbsolutePath());
+            double[] dd = exif.getLatLong();
+            String ImageWidth = exif.getAttribute("ImageWidth");
+            String ExposureProgram = exif.getAttribute("ExposureProgram");
+            String GPSLongitude = exif.getAttribute("GPSLongitude");
+            String GPSVersionID = exif.getAttribute("GPSVersionID");
+
+            String mata = exif.getAttribute(ExifInterface.TAG_DATETIME);
+            int orientation = exif.getRotationDegrees();
 
             GeoDegree geoDegree = new GeoDegree(exif);
             latitude = Double.valueOf(geoDegree.toString().split(", ")[0]);
-            longitude= Double.valueOf(geoDegree.toString().split(", ")[1]);
+            longitude = Double.valueOf(geoDegree.toString().split(", ")[1]);
 
             TextView metadataTextView = findViewById(R.id.meta_data);
-            metadataTextView.setText(geoDegree.toString());
+            //metadataTextView.setText(geoDegree.toString());
+            metadataTextView.setText(ImageWidth + " " + ExposureProgram + " " + GPSLongitude + " " + GPSVersionID + " " + geoDegree.toString());
 
         } catch (ImageProcessingException | IOException e) {
             e.printStackTrace();
@@ -142,11 +164,8 @@ public class Activity3 extends AppCompatActivity {
         }
     }
 
-    public void openMap(View view){
-        Intent intent = new Intent(Activity3.this, Activity5.class);
-        Intent oldIntent = getIntent();
-        ExampleElement el = oldIntent.getParcelableExtra("Example element");
-        intent.putExtra("Example element", el);
+    public void openMap(View view) {
+        Intent intent = new Intent(Activity3.this, Activity4.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/android/example/harmanproject/Activity4.java
+++ app/src/main/java/android/example/harmanproject/Activity4.java
@@ -1,98 +1,142 @@
 package android.example.harmanproject;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.location.Location;
 import android.os.Bundle;
-import android.widget.Button;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
-import com.mapbox.geojson.Feature;
-import com.mapbox.geojson.FeatureCollection;
-import com.mapbox.geojson.LineString;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
-import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
-import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class Activity4 extends AppCompatActivity  {
-    private static final long DEFAULT_MAX_WAIT_TIME = 5000;
-    private static final long DEFAULT_INTERVAL_IN_MILLISECONDS = 3000;
+
+public class Activity4 extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener {
     private MapView mapView;
-    private Mapbox mapbox;
     private MapboxMap mapboxMap;
-    private Button startButton;
-    private LocationEngine locationEngine;
-    private LocationComponent locationComponent;
-    private Location originLocation;
-    private NavigationMapRoute route;
-    private final String TAG = "Activity 4";
-    private Point originPosition;
-    private Point destinationPosition;
+    // variables for adding location layer
     private PermissionsManager permissionsManager;
-    private Marker destinationMarker;
+    private LocationComponent locationComponent;
+    // variables for calculating and drawing a route
+    private DirectionsRoute currentRoute;
+    private static final String TAG = "DirectionsActivity";
+    private NavigationMapRoute navigationMapRoute;
+    // variables needed to initialize navigation
+    private NavigationMapRoute route;
 
-    Point origin = Point.fromLngLat(-77.03613, 38.90992);
-    Point destination = Point.fromLngLat(-77.0365, 38.8977);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
-
         setContentView(R.layout.activity_4);
-
-        mapView = (MapView) findViewById(R.id.mapView);
+        Mapbox.getInstance(this, getString(R.string.MAPBOX_ACCESS_TOKEN));
+        mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(mapboxMap -> {
+        mapView.getMapAsync(this);
+    }
 
-
-            // Map is set up and the style has loaded. Now you can add data or make other map adjustments
-            mapboxMap.setStyle(Style.MAPBOX_STREETS, style -> {
-                Intent intent = getIntent();
-                ExampleElement exampleElement = intent.getParcelableExtra("Example element");
-                Bitmap icon = BitmapFactory.decodeFile(exampleElement.getPath());
-                style.addImage("my-marker-image", icon);
-
-                SymbolLayer symbolLayer = new SymbolLayer("layer-id", "source-id");
-                symbolLayer.setProperties(PropertyFactory.iconImage("my-marker-image"),
-                        iconOffset(new Float[] {(float) -77.03613, (float) 38.90992}));
-
-                style.addLayer(symbolLayer);
-
-                List routeCoordinates = new ArrayList<Point>();
-                routeCoordinates.add(Point.fromLngLat(-118.394391, 33.397676));
-                routeCoordinates.add(Point.fromLngLat(-118.370917, 33.391142));
-
-                // Create the LineString from the list of coordinates and then make a GeoJSON FeatureCollection so that you can add the line to our map as a layer.
-
-                LineString lineString = LineString.fromLngLats(routeCoordinates);
-
-                FeatureCollection featureCollection = FeatureCollection.fromFeature(Feature.fromGeometry(lineString));
-
-                GeoJsonSource geoJsonSource = new GeoJsonSource("geojson-source", featureCollection);
-                style.addSource(geoJsonSource);
-            });
-
+    @Override
+    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
+        mapboxMap.setStyle(getString(R.string.navigation_guidance_day), new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                enableLocationComponent(style);
+                        Point originPoint = Point.fromLngLat(locationComponent.getLastKnownLocation().getLongitude(),
+                 locationComponent.getLastKnownLocation().getLatitude());
+                Point pictureLocation = Point.fromLngLat(Activity3.longitude, Activity3.latitude);
+                getRoute(originPoint, pictureLocation);
+            }
         });
 
     }
 
 
+
+    private void getRoute(Point origin, Point destination) {
+        NavigationRoute.builder(getApplicationContext())
+                .accessToken(Mapbox.getAccessToken())
+                .origin(origin)
+                .destination(destination)
+                .build()
+                .getRoute(new Callback<DirectionsResponse>() {
+                    @Override
+                    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                        if (response.body() == null) {
+                            Log.e(TAG, "No routes found, check right user and access token");
+                            return;
+                        } else if (response.body().routes().size() == 0) {
+                            Log.e(TAG, "No routes found");
+                        }
+                        DirectionsRoute currentRoad = response.body().routes().get(0);
+
+                        if (route != null) {
+                            route.removeRoute();
+                        } else {
+                            route = new NavigationMapRoute(null, mapView, mapboxMap);
+                        }
+                        route.addRoute(currentRoad);
+                    }
+
+                    @Override
+                    public void onFailure(Call<DirectionsResponse> call, Throwable t) {
+                        Log.e(TAG, "Error: " + t.getMessage());
+                    }
+                });
+    }
+
+
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            // Activate the MapboxMap LocationComponent to show user location
+            // Adding in LocationComponentOptions is also an optional parameter
+            locationComponent = mapboxMap.getLocationComponent();
+            locationComponent.activateLocationComponent(this, loadedMapStyle);
+            locationComponent.setLocationComponentEnabled(true);
+            // Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            enableLocationComponent(mapboxMap.getStyle());
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
 }
