a47ab05deec56bab633aadea4abdc0f17126c799
Yuliya <ulya962009@gmail.com>
2020-05-13 19:16:47 +0300

    SSID, RSSi and Mac Address displayed

--- app/src/main/java/android/example/harmanproject/Activity5.java
+++ app/src/main/java/android/example/harmanproject/Activity5.java
@@ -1,14 +1,30 @@
 package android.example.harmanproject;
 
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity5 extends AppCompatActivity {
+    WifiManager wifiManager;
+    WifiInfo connection;
+    String display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_5);
+
+        TextView textView = findViewById(R.id.wifi_info);
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        assert wifiManager != null;
+        connection = wifiManager.getConnectionInfo();
+
+        display = "SSID: " + connection.getSSID() +"\nRSSi: " + connection.getRssi() + "\nMac Address: " + connection.getMacAddress();
+        textView.setText(display);
     }
 }
