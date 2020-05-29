82f99f556ec472abff4d39c825b6cf1f2789cff8
Yuliya <ulya962009@gmail.com>
2020-05-29 11:17:29 +0300

    EnterMessage fragment began to use view binding

--- app/src/main/java/android/example/harmanproject/View/EnterMessage.java
+++ app/src/main/java/android/example/harmanproject/View/EnterMessage.java
@@ -1,7 +1,7 @@
 package android.example.harmanproject.View;
 
 import android.annotation.SuppressLint;
-import android.example.harmanproject.R;
+import android.example.harmanproject.databinding.EnterMessageBinding;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,12 +9,15 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import org.jetbrains.annotations.NotNull;
+
 public class EnterMessage extends Fragment {
 
     @SuppressLint("InflateParams")
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.enter_message, null);
+        EnterMessageBinding binding = EnterMessageBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }
