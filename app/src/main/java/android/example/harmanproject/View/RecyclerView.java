23bf1b966921db71a2fe490588583249280e4bde
Yuliya <ulya962009@gmail.com>
2020-05-29 11:12:18 +0300

    WifiInfo class began to use view binding

--- app/src/main/java/android/example/harmanproject/View/RecyclerView.java
+++ app/src/main/java/android/example/harmanproject/View/RecyclerView.java
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 public class RecyclerView extends Fragment {
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -21,6 +22,7 @@ public class RecyclerView extends Fragment {
         androidx.recyclerview.widget.RecyclerView recyclerView;
         View view = inflater.inflate(R.layout.recycler_fragment, container, false);
         recyclerView = view.findViewById(R.id.recycle_view);
+
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         RecyclerAdapter recyclerAdapter = new RecyclerAdapter(GalleryViewModel.exampleList);
--- app/src/main/java/android/example/harmanproject/View/WifiInfo.java
+++ app/src/main/java/android/example/harmanproject/View/WifiInfo.java
@@ -1,16 +1,16 @@
 package android.example.harmanproject.View;
 
-import android.example.harmanproject.R;
 import android.example.harmanproject.ViewModel.WifiViewModel;
+import android.example.harmanproject.databinding.WifiInfoBinding;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WifiInfo extends AppCompatActivity {
     private WifiViewModel mViewModel;
 
-    public WifiInfo(){
+    public WifiInfo() {
         mViewModel = new WifiViewModel(this);
     }
 
@@ -18,12 +18,10 @@ public class WifiInfo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.wifi_info);
+        WifiInfoBinding binding = WifiInfoBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        TextView textView = findViewById(R.id.wifi_info);
-        textView.setText(mViewModel.findWifiInfo());
-
-
-
+        binding.wifiInfoText.setText(mViewModel.findWifiInfo());
     }
 }
